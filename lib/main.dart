0c93fd52476dc1a1a877a23471872071ddfa23ff
Clarity <enaalmuhammad@gmail.com>
2024-10-03 11:28:47 +0700

    food_application

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:food_app_prokit/screen/FoodWalkThrough.dart';
+import 'package:food_app_prokit/store/AppStore.dart';
+import 'package:food_app_prokit/utils/AppTheme.dart';
+import 'package:food_app_prokit/utils/FoodConstants.dart';
+import 'package:food_app_prokit/utils/FoodDataGenerator.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+
+
+AppStore appStore = AppStore();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initialize(aLocaleLanguageList: languageList());
+
+  appStore.toggleDarkMode(value: getBoolAsync(isDarkModeOnPref));
+
+  defaultToastGravityGlobal = ToastGravity.BOTTOM;
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Food App${!isMobile ? ' ${platformName()}' : ''}',
+        home: FoodWalkThrough(),
+        theme: !appStore.isDarkModeOn ? AppThemeData.lightTheme : AppThemeData.darkTheme,
+        navigatorKey: navigatorKey,
+        scrollBehavior: SBehavior(),
+        supportedLocales: LanguageDataModel.languageLocales(),
+        localeResolutionCallback: (locale, supportedLocales) => locale,
+      ),
+    );
+  }
+}
